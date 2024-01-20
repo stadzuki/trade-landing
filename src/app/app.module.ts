0a26e4af684f2aee4e1c7238f34f816511183e7d
Artem Laziuk <artem.l@datarails.com>
2024-01-21 01:51:18 +0400

    feat: setting up bootstrap application, remove router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-import { AppRouterModule } from './app-router.module';
+import { HeaderComponent } from '@app/@standalone/header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
+        BrowserModule,
         CommonModule,
-        AppRouterModule
-    ]
+        HeaderComponent
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
