528bf5d7a038cee94a8fd0085ddeff6c373a4421
Artem Laziuk <artem.l@datarails.com>
2024-01-21 18:11:00 +0400

    feat: display main component & store standalone components in arr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '@app/@standalone/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { MainComponent } from '@app/@standalone/main/main.component';
+
+const STANDALONE_COMPONENTS = [
+    HeaderComponent,
+    MainComponent
+];
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
         CommonModule,
-        HeaderComponent
+        ...STANDALONE_COMPONENTS
     ],
     bootstrap: [AppComponent],
 })
