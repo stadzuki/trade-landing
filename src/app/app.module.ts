60e71924fe42597b6ca56f66dfb6f9368e60aab6
Artem Laziuk <artem.l@datarails.com>
2024-01-22 00:40:26 +0400

    feat: display crypto list on the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from '@app/@standalone/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainComponent } from '@app/@standalone/main/main.component';
+import { CryptoBadgeComponent } from '@app/@standalone/crypto-badge/crypto-badge.component';
+import { CryptoListModule } from '@app/@shared/crypto-list/crypto-list.module';
 
 const STANDALONE_COMPONENTS = [
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    CryptoBadgeComponent,
 ];
 
 @NgModule({
@@ -15,6 +18,7 @@ const STANDALONE_COMPONENTS = [
     imports: [
         BrowserModule,
         CommonModule,
+        CryptoListModule,
         ...STANDALONE_COMPONENTS
     ],
     bootstrap: [AppComponent],
